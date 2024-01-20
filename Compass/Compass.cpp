851c59ec45f6e1a27c4d504a29e9e5f2f6cefcd9
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-01-20 23:44:30 +0100

    Speed up

--- Compass/Compass.cpp
+++ Compass/Compass.cpp
@@ -1,7 +1,7 @@
 // Lic:
 // Dev/Luna Development Tools/Compass/Compass.cpp
-// Version: 23.12.06
-// Copyright (C) 2023 Jeroen Petrus Broks
+// Version: 24.01.17
+// Copyright (C) 2023, 2024 Jeroen Petrus Broks
 // 
 // ===========================
 // This file is part of a project related to the Phantasar Chronicles or another
@@ -54,10 +54,12 @@ using namespace Kthura;
 
 string myexe{}, myexedir{}, datafile{}, kthuradir{}, compassoutdir{};
 GINIE Data{ nullptr };
+GINIE Changed{ nullptr };
+bool Forced{ false };
 
 bool AllowedTag(string Tag) {
 	Trans2Upper(Tag);
-	vector<string> Forbidden{ "ZONE", "CHEST", "RNDITEM", "RANDOM_TREASURE", "SAVE", "PASS", "TRAVELERSEMBLEM", "MANAMENT_CRYSTAL_", "BLACKORB_","TOME_","BREAK_","CRACK_","CRTEX_" };
+	vector<string> Forbidden{ "ZONE", "CHEST", "RNDITEM", "RANDOM_TREASURE", "SAVE", "PASS", "TRAVELERSEMBLEM", "MANAMENT_CRYSTAL_", "BLACKORB_","TOME_","BREAK_","CRACK_","CRTEX_","SCYNDIPOLE" };
 	for (auto chk : Forbidden) if (Prefixed(Tag, chk)) return false;
 	return true;
 }
@@ -84,54 +86,61 @@ void AddTag(GINIE d, KthuraObject* o, string laytag, string name) {
 }
 
 void ScanMap(string mapfile) {
-	GINIE outdata{ ParseGINIE("[***]\nLastModified=" + CurrentDate() + "; " + CurrentTime()) };
-	Data->NewValue("GlobalTags", "Next", "Yes");
-	Data->NewValue("GlobalTagName", "Next", "Forward");
-	Data->NewValue("GlobalTags", "Prev", "Yes");
-	Data->NewValue("GlobalTagName", "Prev", "Backward");	
-	QCol->Doing("Processing", mapfile);
-	auto kMap{ LoadKthura(kthuradir+"/"+mapfile)};
-	auto lays{ *kMap->Layers() };
-	for (auto laytag : lays) {
-		auto lay = (*kMap)[laytag];
-		QCol->Doing("=> Layer", laytag);
-		for (auto o = lay->FirstObject(); o; o = o->Next()) {
-			QCol->Doing("=> Object", o->ID(), "\r");
-			auto Tag = o->Tag();
-			auto LocalTag{ "LOCALTAG::" + mapfile + "::" + laytag + "::" + Tag };
-			if (Tag.size() && AllowedTag(Tag)) {
-				if (!Data->Value("GlobalTags", Tag).size()) {
-					QCol->Red("Uknown item\n");
-					QCol->Doing("Object", o->ID());
-					QCol->Doing("Kind", o->SKind());
-					QCol->Doing("Coords", TrSPrintF("(%d,%d)", o->x(), o->y()));
-					QCol->Doing("Size", TrSPrintF("%dx%d", o->w(), o->h()));
-					QCol->Doing("Layer", laytag);
-					QCol->Doing("Map", mapfile);
-					QCol->Magenta("What to do?\n");
-				}
-				if (Yes(Data, "GlobalTags", Tag, "Is tag \"" + Tag + "\" a global tag")) {
-					AddTag(outdata, o, laytag, Ask(Data, "GlobalTagName", Tag, "Description of global tag \"" + Tag + "\":"));
-				} else if (!Yes(Data,"GlobalIgnore",Tag,"IGNORE \""+Tag+"\" globally then")) {
-					if (Yes(Data, LocalTag, "Use", "Use \"" + Tag + "\" as a local tag then")) {
-						AddTag(outdata, o, laytag, Ask(Data, LocalTag, "Name", "Name for local tag \"" + Tag + "\":"));
+	auto SaveFile{ compassoutdir + "/" + mapfile + "/Compass.ini" };
+	if ((!FileExists(SaveFile)) || Forced || Changed->IntValue(mapfile, "Size") != FileSize(kthuradir + "/" + mapfile) || FileTimeStamp(kthuradir + "/" + mapfile) != Changed->IntValue(mapfile, "Time")) {
+		GINIE outdata{ ParseGINIE("[***]\nLastModified=" + CurrentDate() + "; " + CurrentTime()) };
+		Data->NewValue("GlobalTags", "Next", "Yes");
+		Data->NewValue("GlobalTagName", "Next", "Forward");
+		Data->NewValue("GlobalTags", "Prev", "Yes");
+		Data->NewValue("GlobalTagName", "Prev", "Backward");
+		QCol->Doing("Processing", mapfile);
+		auto kMap{ LoadKthura(kthuradir + "/" + mapfile) };
+		auto lays{ *kMap->Layers() };
+		for (auto laytag : lays) {
+			auto lay = (*kMap)[laytag];
+			QCol->Doing("=> Layer", laytag);
+			for (auto o = lay->FirstObject(); o; o = o->Next()) {
+				QCol->Doing("=> Object", o->ID(), "\r");
+				auto Tag = o->Tag();
+				auto LocalTag{ "LOCALTAG::" + mapfile + "::" + laytag + "::" + Tag };
+				if (Tag.size() && AllowedTag(Tag)) {
+					if (!Data->Value("GlobalTags", Tag).size()) {
+						QCol->Red("Uknown item\n");
+						QCol->Doing("Object", o->ID());
+						QCol->Doing("Kind", o->SKind());
+						QCol->Doing("Coords", TrSPrintF("(%d,%d)", o->x(), o->y()));
+						QCol->Doing("Size", TrSPrintF("%dx%d", o->w(), o->h()));
+						QCol->Doing("Layer", laytag);
+						QCol->Doing("Map", mapfile);
+						QCol->Magenta("What to do?\n");
+					}
+					if (Yes(Data, "GlobalTags", Tag, "Is tag \"" + Tag + "\" a global tag")) {
+						AddTag(outdata, o, laytag, Ask(Data, "GlobalTagName", Tag, "Description of global tag \"" + Tag + "\":"));
+					} else if (!Yes(Data, "GlobalIgnore", Tag, "IGNORE \"" + Tag + "\" globally then")) {
+						if (Yes(Data, LocalTag, "Use", "Use \"" + Tag + "\" as a local tag then")) {
+							AddTag(outdata, o, laytag, Ask(Data, LocalTag, "Name", "Name for local tag \"" + Tag + "\":"));
+						}
 					}
 				}
 			}
+			Changed->Value(mapfile, "Size", FileSize(kthuradir + "/" + mapfile));
+			Changed->Value(mapfile, "Time", FileTimeStamp(kthuradir + "/" + mapfile));
 		}
+
+
+		
+		auto SaveDir{ ExtractDir(SaveFile) };
+		if (!DirectoryExists(SaveDir)) {
+			QCol->Doing("=> Creating", SaveDir);
+			MakeDir(SaveDir);
+		}
+		QCol->Doing("=> Saving", SaveFile);
+		Units::SaveString(SaveFile, outdata->UnParse("Compass data for: " + mapfile + "\n(c) Jeroen P. Broks!\nDo NOT extract this file from the project without prior written permission!"));
 	}
-	auto SaveFile{ compassoutdir + "/" + mapfile + "/Compass.ini" };
-	auto SaveDir{ ExtractDir(SaveFile) };
-	if (!DirectoryExists(SaveDir)) {
-		QCol->Doing("=> Creating", SaveDir);
-		MakeDir(SaveDir);
-	}
-	QCol->Doing("=> Saving", SaveFile);
-	SaveString(SaveFile, outdata->UnParse("Compass data for: " + mapfile + "\n(c) Jeroen P. Broks!\nDo NOT extract this file from the project without prior written permission!"));
 }
 
 
-int main(int,char** args) {
+int main(int ca,char** args) {
 	init_zlib();
 	JCR6_InitRealDir();
 	QCol->LGreen("Compass for Luna's Father!\n");
@@ -140,6 +149,8 @@ int main(int,char** args) {
 	datafile = TrSPrintF("%s/Compass.ini", myexedir.c_str());
 	if (!FileExists(datafile)) SaveString(datafile,"[System]\nCreated:" + CurrentDate());
 	Data = LoadGINIE(datafile,datafile,"Compass config data!\n Last modified:"+CurrentDate()+"\n(c) Jeroen P. Broks");
+	Changed = LoadOptGINIE(StripExt(myexe) + ".Changed.ini", StripExt(myexe) + ".Changed.ini", "Change log!");
+	for (size_t i = 1; i < ca; ++i) Forced = Forced || Upper(args[i]) == "FORCE";
 	kthuradir = Ask(Data, "Directories", "Kthura", "Where can I find the Kthura files?");
 	QCol->Doing("Analysing", kthuradir);
 	auto kthuramaps{ *FileList(kthuradir) };
